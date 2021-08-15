81a213ae3996e70c58c936a4d32124c403d2cd99
schliemannosaurusrex <daniel.schliemann@googlemail.com>
2021-08-16 00:01:46 +0200

    remove .cbr to get *.pdf only; added skipping existing results (*.pdf)

--- cb2pdf.sh
+++ cb2pdf.sh
@@ -5,23 +5,38 @@ echo "Original: "$ORIGINAL_FOLDER
 
 #TODO create folder "converted2Pdf" if not already existant
 
-JPEGS=$(mktemp -d)
-echo "Temp: "$JPEGS
+FILE="${ORIGINAL_FOLDER}/${1}.pdf"
 
-cp "$1" "$JPEGS/$1"
-cd "$JPEGS"
-
-if [ ${1: -4} == ".cbr" ]
+if [[ -f $FILE ]];
 then
-    unrar e "$1"
+    echo "'$1.pdf' already exists ;)"
 else
-    unzip -j "$1" -d "$JPEGS"
+    JPEGS=$(mktemp -d)
+	echo "Temp: "$JPEGS
+
+	cp "$1" "$JPEGS/$1"
+	cd "$JPEGS"
+
+	if [ ${1: -4} == ".cbr" ]
+	then
+		extension=.cbr
+		unrar e "$1"
+	else
+		extension=.cbz
+		unzip -j "$1" -d "$JPEGS"
+	fi
+
+	ls -1 ./*jp*g | xargs -L1 -I {} img2pdf {} -o {}.pdf
+	pdftk *.pdf cat output combined.pdf
+	
+	#remove .cbr to get .pdf only
+	filename=$(basename "$1" $extension)
+	cp "combined.pdf" "$ORIGINAL_FOLDER/$filename.pdf"
+	cd ..
+
+	# cleanup
+	rm -R $JPEGS
+	rm "${ORIGINAL_FOLDER}/${1}"
 fi
 
-ls -1 ./*jp*g | xargs -L1 -I {} img2pdf {} -o {}.pdf
-pdftk *.pdf cat output combined.pdf
-cp "combined.pdf" "$ORIGINAL_FOLDER/$1.pdf"
-cd ..
 
-# cleanup
-rm -R $JPEGS
--- convertComicBookArchives2Pdf.sh
+++ convertComicBookArchives2Pdf.sh
@@ -3,6 +3,11 @@
 # USAGE: Run this script in the directory containing cbr|cbz files!
 baseDir=$(dirname "$0")
 
+# TODO add feature "delete source file -> -d as option ($1) for this script"
+#
+# Not much XP with XARGS. Need to get more infos how to xtend
+# 'xargs -L1 -I{} $baseDir/cb2pdf.sh {}' correctly
+
 # running cb2pdf.sh using filtered list of parameters:
 # filtering: for cbr | cbz files -> "[cbr|cbz]$"
 tree -fai . | grep -P "[cbr|cbz]$" | xargs -L1 -I{} $baseDir/cb2pdf.sh {}
